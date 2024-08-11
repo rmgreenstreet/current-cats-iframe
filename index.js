58d3f406d298ff9fce07da8846ad074bbe4557df
Robert Greenstreet <robertgreenstreet@gmail.com>
2024-08-10 22:58:58 -0500

    Proof of concept for selecting cats to make flyers for, and potentially structuring a page to convert into a pdf

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import ky from "ky";
+import ejs from "ejs";
+import pdfkit from "pdfkit";
 import ejsMate from "ejs-mate";
 
 import asyncWrapper from "./utils/asyncWrapper.js";
@@ -14,6 +16,8 @@ const __filename = fileURLToPath(import.meta.url); // get the resolved path to t
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 const app = express();
+
+app.use(express.urlencoded({extended : true}));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -27,6 +31,19 @@ app.get("/tcc/cats", async (req, res) => {
     const data = await ky.get(`https://petstablished.com/api/v2/public/pets?public_key=${process.env.PUBLIC_KEY}&pagination[limit]=100&search[status]=Available`).json();
     // console.log(data.collection[0]);
     res.render("iframe", {data});
+});
+
+app.get("/tcc/flyers", async (req, res) => {
+    const data = await ky.get(`https://petstablished.com/api/v2/public/pets?public_key=${process.env.PUBLIC_KEY}&pagination[limit]=100&search[status]=Available`).json();
+    // console.log(data.collection[0]);
+    res.render("flyers", {data});
+});
+
+app.post("/tcc/flyers", async (req, res) => {
+    const listOfCats = req.body.selected;
+    const data = await ky.get(`https://petstablished.com/api/v2/public/pets?public_key=${process.env.PUBLIC_KEY}&pagination[limit]=100&search[status]=Available&search[name]=${listOfCats.join(",")}`).json();
+    // console.log(data.collection[0]);
+    res.render("individualFlyer", {data});
 });
 
 app.all("*", (req, res) => {
