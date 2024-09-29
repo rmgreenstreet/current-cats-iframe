567db6761c312966b4ca5e3ffe369dc59dff0f68
Robert Greenstreet <robertgreenstreet@gmail.com>
2024-09-29 00:10:24 -0500

    Update formatting of age display to account for singular units of time

--- utils/formatAge.mjs
+++ utils/formatAge.mjs
@@ -14,15 +14,15 @@ export default function formatAge(birthDate) {
     const diffDays = now.getDate() - jsBirthDate.getDate();
     const weeks = Math.floor(diffDays / 7);
   
-    let yearsAndMonths = `${years} years and ${months} months`;
-    let monthsAndWeeks = `${months} months and ${weeks} weeks`;
+    let yearsAndMonths = `${years} year${years > 1 ? "s" : ""} and ${months} month${months > 1 ? "s" : ""}`;
+    let monthsAndWeeks = `${months} month${months > 1 ? "s" : ""} and ${weeks} week${weeks > 1 ? "s" : ""}`;
 
     if (months < 1) {
-      yearsAndMonths = `${years} years`
+      yearsAndMonths = `${years} year${years > 1 ? "s" : ""}`
     }
 
     if (weeks < 1) {
-      monthsAndWeeks = `${months} months`
+      monthsAndWeeks = `${months} month${months > 1 ? "s" : ""}`
     }
   
     return years >= 1 ? yearsAndMonths : monthsAndWeeks;
