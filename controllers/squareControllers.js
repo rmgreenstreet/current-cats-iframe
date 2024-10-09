37b931bfa861d374e9c4621bbffb208994c7557e
Robert Greenstreet <robertgreenstreet@gmail.com>
2024-10-09 16:31:26 -0500

    Not sure what changed

--- controllers/squareControllers.js
+++ controllers/squareControllers.js
@@ -399,7 +399,7 @@ const onDemandDisplay = async (req, res, next) => {
         const startTime = new Date(Date.now()).toLocaleTimeString();
         const finishTime = new Date(Date.now() + expectedTime * 60 * 1000).toLocaleTimeString();
         res.send(`<div style="margin-left: 20%; margin-top: 3em"><h1>Loyalty Points Update Started</h1> <h2>${loyaltyAccountsList.length} Loyalty Accounts To Process</h2><p>This page will not update after loyalty points have been processed.</p><p>Accounts take about 2 seconds each to process, so please allow at least ${expectedTime} minutes</p><p>Started: ${startTime}. Expected Completion: ${finishTime}.</p></div>`);
-        // await correctLoyaltyPoints();
+        await correctLoyaltyPoints();
     } catch (err) {
         console.log(err);
         res.send("There was an issue updating loyalty account point amounts. Please refresh the page to try again. If the issue persists, contact robertgreenstreet@gmail.com");
